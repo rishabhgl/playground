f6cb2f9b2278e70009c596877b63844c263114fe
rishabhgl <rishabhisjayden@gmail.com>
2024-02-14 10:36:39 +0530

    Added customizability

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -1,45 +1,53 @@
 class TicTacToe():
 
-    def __init__(self, piece_one = "O", piece_two = "X"):
-        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    def __init__(self, piece_one = "O", piece_two = "X", size = 3):
+        self.size = size
+        self.board = [[" "] * size for i in range(size)]
         self.player_pieces = (piece_one, piece_two)
         self.current = 1
 
     def check_win(self):
 
-        if self.board[0][0] == self.board[0][1] and self.board[0][1] == self.board[0][1] and self.board[0][0] != " ":
-            return True
-        if self.board[1][0] == self.board[1][1] and self.board[1][1] == self.board[1][2] and self.board[1][0] != " ":
-            return True
-        if self.board[2][0] == self.board[2][1] and self.board[2][1] == self.board[2][2] and self.board[2][0] != " ":
-            return True
-        if self.board[0][0] == self.board[1][0] and self.board[1][0] == self.board[2][0] and self.board[0][0] != " ":
-            return True
-        if self.board[0][1] == self.board[1][1] and self.board[1][1] == self.board[2][1] and self.board[0][1] != " ":
-            return True
-        if self.board[0][2] == self.board[1][2] and self.board[1][2] == self.board[2][2] and self.board[0][2] != " ":
-            return True
-        if self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[1][1]and self.board[0][0] != " ":
-            return True
-        if self.board[0][2] == self.board[1][1] and self.board[1][1] == self.board[2][0] and self.board[0][2] != " ":
+        sum_rows = [0] * self.size
+        sum_cols = [0] * self.size
+        sum_diagonals = [0, 0]
+
+        for i in range(self.size):
+            for j in range(self.size):  
+                if self.board[i][j] == self.player_pieces[self.current]:
+                    sum_rows[i] += 1
+                    sum_cols[j] += 1
+                    if i == j:
+                        sum_diagonals[0] += 1
+                    if j == self.size - 1 - i:
+                        sum_diagonals[1] += 1
+
+        if sum_diagonals[0] == self.size or sum_diagonals[1] == self.size:
             return True
         
+        for i in range(self.size):
+            if sum_cols[i] == self.size or sum_rows[i] == self.size:
+                return True
+
         self.current = (self.current + 1) % 2
         return False
 
     def make_move(self, pos):
 
-        if self.board[pos[0]][pos[1]] == " ":
+        out_of_bounds = pos[0] >= self.size or pos[0] < 0 or pos[1] >= self.size or pos[1] < 0
+        if not out_of_bounds and self.board[pos[0]][pos[1]] == " ":
             self.board[pos[0]][pos[1]] = self.player_pieces[self.current]
-            return "Good move!"
+            return ""
         else:
-            return "Invalid move! Try again"
+            self.current = (self.current + 1) % 2
+            return "Invalid move! Try again~"
             
     def print_board(self):
-        for i in range(3):
-            print("---------")
-            for j in range(3):
+        border = "---" * self.size
+        for i in range(self.size):
+            print(border)
+            for j in range(self.size):
                 print(f"|{self.board[i][j]}|", end = "")
             print("")
-            print("---------")
+            print(border)
     
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from games.game import TicTacToe
+from games.tictactoe import TicTacToe
 
 if __name__ == "__main__":
-
-    game = TicTacToe()
+    size = int(input("Enter the size of the board you want to play on: "))
+    piece_zero = input("Player zero, enter the piece you want to play with (any one character): ")
+    piece_one = input("Player one, enter the piece you want to play with: ")
+    game = TicTacToe(piece_zero, piece_one, size)
 
     while not game.check_win():
         game.print_board()
